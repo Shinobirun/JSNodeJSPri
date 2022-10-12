f997a618ad034c632cad317cfce45817073def37
Shinobirun <adrian.latorre@gmail.com>
2022-10-12 12:49:19 -0300

    segundoejercicio

---
+++ JsEjercicioArray.js
@@ -0,0 +1,15 @@
+const lista = [2, 3, 4, 5, 6, 7];
+
+const fecha_naci = new Date(1972, 4, 29);
+
+
+const libroFav = {
+    titulo: "Rayuela",
+    autor: "Cortazar"
+    anio: new Date(1972),
+    url: "https://www.amazon.es/Rayuela-hinkelspel-roman-Julio-Cortazar/dp/9029089466/ref=sr_1_2?crid=3RWVDA2TL02K9&keywords=rayuela+julio+cortazar&qid=1665589264&qu=eyJxc2MiOiIxLjAwIiwicXNhIjoiMC42MCIsInFzcCI6IjAuNTkifQ%3D%3D&sprefix=rayuela%2Caps%2C287&sr=8-2"
+}
+
+const lista2 = ["Adri", 50, true, fecha_naci, libroFav];
+
+console.log(lista2)
